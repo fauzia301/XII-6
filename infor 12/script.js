453e71b721a837f73cde9c975897a7f1b43d8349
fauzia301 <fiptiara@gmail.com>
2024-10-14 08:27:09 +0700

    bebas

---
+++ infor 12/script.js
@@ -0,0 +1,14 @@
+let jmlruangan = 7;
+let noruang = 1;
+let kelasberfungsi = 3;
+
+for (noruang; noruang<= kelasberfungsi; noruang++); {
+    if (noruang<= kelasberfungsi) {
+        console.log (`speaker ruang ${noruang} berfungsi`);
+    } else if (noruang == 4) {
+        console.log (`speaker ruang ${noruang} sedang diperbaiki`);
+    } else {
+        console.log (`speaker ruang ${noruang} tidak berfungsi`);
+    }    
+}
+
